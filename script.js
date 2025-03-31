dc3daed4401d658d0e09e5f7db6a7a0126506ade
PDong2501 <pdongdz191@gmail.com>
2025-03-31 12:25:04 +0700

    Them hieu ung va thong bao tieu de

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Simple Web Project loaded!");
 });
+
+document.querySelector("h1").addEventListener("click", function(){
+    alert("Ban da nhap vao tieu de");
+});
